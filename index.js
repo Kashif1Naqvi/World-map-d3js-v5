6985d8e80856ae749fd31d266fe59ba2728b9765
Kashif1Naqvi <40764353+Kashif1Naqvi@users.noreply.github.com>
2019-12-10 19:38:46 +0500

    code updated

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
-let svg = d3.select("#map").append("svg").attr("height",600).attr("width",800).style("margin-top","80px")
-let projection = d3.geoMercator()
-let pathGenrator = d3.geoPath().projection(projection)
-svg.append("path").attr('class', 'sphere').attr("d",pathGenrator({type:'Sphere'}))
 
+let svg = d3.select("#map")
+            .append("svg")
+            .attr("viewBox",`0 0 ${1000} ${600}`)
+            .style("margin-top","80px")
+
+let projection = d3.geoNaturalEarth1()
+
+let pathGenrator = d3.geoPath().projection(projection)
+
+svg
+  .append("path")
+  .attr('class', 'sphere')
+  .attr("d",pathGenrator({type:'Sphere'}))
 d3.json("data.json").then(data=>{
-    let country  = topojson.feature(data,data.objects.countries)
-    svg.selectAll("path").data(country.features).enter().append("path").attr("d",pathGenrator)
+    let country = topojson.feature(data,data.objects.countries)
+    svg.selectAll("path")
+      .data(country.features)
+      .enter()
+      .append("path")
+      .attr("d",pathGenrator)
+      .attr("class","country")
 })
