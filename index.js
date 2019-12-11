2b104d375cdb88bc62676ee2ad18d88e23fa1adf
Kashif1Naqvi <40764353+Kashif1Naqvi@users.noreply.github.com>
2019-12-11 18:02:03 +0500

    code updated

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
-
+let height = 500;
+let width = 1000;
 let svg = d3.select("#map")
             .append("svg")
-            .attr("viewBox",`0 0 ${1000} ${600}`)
-            .style("margin-top","80px")
-
+            .attr("class","container-fluid")
+            .attr("viewBox",[`0 0 ${width} ${height}`])
+/*
+ other projection
+    d3.geoAzimuthalEqualArea()
+    d3.geoAzimuthalEquidistant()
+    d3.geoStereographic()
+    d3.geoConicConformal()
+    d3.geoConicEqualArea()
+    d3.geoConicEquidistant()
+*/
 let projection = d3.geoNaturalEarth1()
-
 let pathGenrator = d3.geoPath().projection(projection)
 const tooltips    = d3.select("#map").append("div").attr("class","tooltips");
-svg
-  .append("path")
+svg.append("path")
   .attr('class', 'sphere')
   .attr("d",pathGenrator({type:'Sphere'}))
 d3.json("data.json").then(data=>{
@@ -26,8 +33,18 @@ d3.json("data.json").then(data=>{
       .attr("fill",(d,i)=>color[i])
       .on("mouseover",function(d,i){
         tooltips.html("<div>"+map[i]+"</div>")
-                .style("top",(d3.event.pageY - 70) + "px")
-                .style("left",(d3.event.pageX - 90) + "px")
+                .style("top",(d3.event.pageY - 33) + "px")
+                .style("left",(d3.event.pageX - 40) + "px")
+        d3.select(this)
+           .style("stroke-width",3)
+           .style("stroke","black")
+           .style("stroke-opacity",0.7)
       })
-
+      .on("mouseout",function(d,i){
+        tooltips.html("")
+        d3.select(this)
+           .style("stroke-width",1)
+           .style("stroke","black")
+           .style("stroke-opacity",0.5)
+      })
 })
